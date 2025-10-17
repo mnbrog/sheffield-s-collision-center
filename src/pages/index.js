d2eea4c7152a766e0443de48c90bd8c729bef1b6
unknown <mnbrogan32@gmail.com>
2025-10-17 13:58:37 -0500

    pc

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,13 +5,35 @@ import ReviewCarousel from '../components/ReviewCarousel';
 import QuickContactBar from '../components/QuickContactBar';
 import SEO from '../components/SEO';
 import reviews from '../../data/reviews.json';
+import locationPhoto from '../../images/IMG_1983.jpeg';
+import styled from 'styled-components';
 
+const ImageSection = styled.section`
+  margin: 3rem auto;
+  max-width: 960px;
+  padding: 0 1rem;
+  text-align: center;
+`;
+
+const LocationImage = styled.img`
+  border-radius: 12px;
+  box-shadow: 0 12px 30px rgba(0, 0, 0, 0.2);
+  width: 100%;
+  height: auto;
+`;
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" description="Collision repair you can trust" />
     <Hero />
     <ReviewCarousel reviews={reviews} />
-    
+       <ImageSection>
+      <LocationImage
+        src={locationPhoto}
+        alt="Exterior of Sheffield's Collision Center"
+        loading="lazy"
+      />
+    </ImageSection>
+
   </Layout>
 );
 
